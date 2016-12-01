20c75a2ce939f59b3aba9a9036378da9271d0634
fjruiz <pajarrurro@gmail.com>
2016-12-01 10:35:32 +0100

    fallo

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,6 +3,8 @@ package principal;
 import java.io.IOException;
 import com.google.gson.Gson;
 import okio.*;
+import principal.BoletoCallback;
+import principal.BoletoInterface;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
